5f77633d34791ca939358ce3b790c448013ad9be
Mark MacGregor <mrm198@hotmail.com>
2019-08-13 21:11:58 +0100

    Added events app

---
+++ events/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+
+class EventIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class EventPage(Page):
+    date = models.DateField("Event date")
+    place = models.CharField(max_length=250)
+    agenda = RichTextField(blank=True)
+    notes = RichTextField(blank=True)
+    resources = RichTextField(blank=True)
+    attended = models.IntegerField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('agenda'),
+        index.SearchField('notes'),
+        index.SearchField('resources'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('place'),
+        FieldPanel('agenda', classname="full"),
+        FieldPanel('notes', classname="full"),
+        FieldPanel('resources', classname="full"),
+        FieldPanel('attended'),
+    ]
