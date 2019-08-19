b8cf469c0594509d67c5fa31cd5e219976fd5aa9
Dave Edwards <dave@dwedigital.com>
2019-08-19 19:13:55 +0100

    added events to the admin interface

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
-
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register,
+)
+from .models import EventPage
 # Register your models here.
+class EventAdmin(ModelAdmin):
+    model = EventPage
+    menu_label = "Events"
+    menu_icon = "date"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer=False
+    list_display = ("date","place")
+    search_fields = ("date", "place")
+
+modeladmin_register(EventAdmin)
