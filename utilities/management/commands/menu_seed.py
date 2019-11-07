5ae7970d6788b63c0a63fdaf6584f144a6180d4b
Dave Edwards <dave@Daves-MacBook-Pro.local>
2019-11-07 18:07:08 +0000

    added error handling to menu seeding mgmg command

--- utilities/management/commands/menu_seed.py
+++ utilities/management/commands/menu_seed.py
@@ -1,12 +1,19 @@
 from django.core.management.base import BaseCommand
 from menus.models import Menu, MenuItem
+import sys
 
 class Command(BaseCommand):
     help = 'Seeds the main-nav menu'
 
     def handle(self, *args, **kwargs):
-        menu = Menu(title='main-nav',slug='main-nav')
-        menu.save()
-        link1 = MenuItem(link_title='test',link_url='https://www.google.com',open_in_new_tab=True,page=menu)
-        link1.save()
+        try:
+            menu = Menu.objects.create(title='main-nav',slug='main-nav')
+            link1 = MenuItem.objects.create(link_title='test',link_url='https://www.google.com',open_in_new_tab=True,page=menu)
+
+            self.stdout.write(self.style.SUCCESS('Menu created, you trooper!'))
+        
+        except:
+            self.stdout.write(self.style.ERROR(f'An error occured: \n {sys.exc_info()[0]}\n{sys.exc_info()[1]}'))
+
+
 
