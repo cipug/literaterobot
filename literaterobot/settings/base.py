b3050c1bc3aff2a601657634ce71136e9ff2cd7d
Rob Dudley <rob@openrock.co>
2020-07-03 11:55:20 +0100

    Fixed leading slash on static asset path

--- literaterobot/settings/base.py
+++ literaterobot/settings/base.py
@@ -188,3 +188,35 @@ WAGTAIL_SITE_NAME = "literaterobot"
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'https://cipug.org'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
